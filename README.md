ac55849da28479c7f8a1d2283eb2618f5f4e2536
Wesley Handy <wesleylhandy@gmail.com>
2017-02-24 19:52:54 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # twitter-bot
 A node.js bot designed to respond to new followers and mentions on a given account.
+
+## Installation Instructions
+
+1. Fork this Repo and Clone your forked copy to your local machine
+2. Run `npm install`
+3. Create a file called `config.js`.
+4. Go to https://dev.twitter.com and create a new App. After the app is created, create new user tokens. Save your keys in tokens in `config.js` according to the following example:
+
+
+```javascript
+exports.twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>'
+}
+```
+
+5. In the bots.js file, where you see the screen_name defined as `wesleylhandy`, which is my twitter screen name, and you should [follow me](https://twitter.com/WesleyLHandy), by the way, replace my screen name with your own.
+6. From the command-line, initiliaze heroku for deployment: `heroku git:remote -a <your heroku app name>`. Then run `git push heroku master`
+
+### What does it do?
+
+This bot is programmed to automatically tweet a thank you message to anyone who adds you as a follower (be sure to change your response message in the `bots.js` file) or to automatically reply to any those who reply to one of your messages. **TODO: add functionality for automatically thanking users for RTs.**
---
+++ bot.js
@@ -0,0 +1,57 @@
+const config = require("./config.js");
+const Twitter = require("twit");
+const fs = require("fs");
+
+var client = new Twitter(config.twitterKeys);
+var stream = client.stream("user");
+
+function postTweet(tweet) {
+	client.post('statuses/update', tweet, function (error, tweet, response) {
+		if (err) {
+			console.log("Something went Wrong!");
+		} else {
+			console.log("Tweet Posted Successfully");
+		}
+	});
+}
+
+var delay;
+
+//listen on twitter stream for new follows
+
+stream.on('follow', followed);
+
+function followed (event) {
+	console.log("New Follow");
+	var name = event.source.name;
+	var screen_name = event.source.screen_name;
+	var newTweet = {status: `@${screen_name} Thank you for following me! Are you into coding too? This is a #nodejsautoreply`};
+	delay = setTimeout(postTweet(newTweet), 4000);
+}
+
+stream.on('tweet', tweeted);
+
+function tweeted (event) {
+	console.log("New Tweet");
+	var data = JSON.stringify(data, null, 5);
+	fs.writeFile('botlog.txt', data, (err)=>{
+		if (err){
+			console.log("error occurred writing file: " + err);
+		}	
+	});
+
+	var replyto = event.in_reply_to_screen_name;
+	var tweetText = event.text;
+	var from = event.user.screen_name;
+
+	console.log(`${from} tweeted to your stream this message: ${tweetText}`);
+
+	if(replyto == "wesleylhandy") {
+		var newTweet = {status: `@${from} Thank you for tweeting me! This is a #nodejsautoreply`}
+		delay = setTimeout(postTweet(newTweet), 4000);
+	}
+}
+  
+stream.on('error', function(error) {
+	console.log("Stream" + error);
+});
