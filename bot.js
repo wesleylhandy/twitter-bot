9eed622be3ae664c363543a997560db2908c10d7
wesleylhandy <wesleylhandy@gmail.com>
2018-01-02 14:48:19 -0500

    added dotenv and environment variables, removed var for const and let, added OOP for randomized reply messages

--- bot.js
+++ bot.js
@@ -1,215 +1,220 @@
-const config = require("./config.js");
 const Twitter = require("twit");
-const fs = require("fs");
 
-var client = new Twitter(config.twitterKeys);
-var stream = client.stream("user");
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load();
+}
+
+const twitterKeys = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+}
+
+const client = new Twitter(twitterKeys);
+const stream = client.stream("user");
 
 const myId = 'WesleyLHandy';
 
-
+console.log('...listening, stalking, whatever, you be the judge...');
 /***********************************/
 /****** RESTFUL API FUNCTIONS ******/
 /***********************************/
 
 function postTweet(tweet) {
-	client.post('statuses/update', tweet, function(error, data, response) {
-		if (error) {
-			console.log("Something went Wrong! " + error);
-		} else {
-			console.log("Tweet Posted Successfully");
-		}
-	});
+    client.post('statuses/update', tweet, function(error, data, response) {
+        if (error) {
+            console.error(`Something went Wrong! ${error}`);
+        } else {
+            console.log("Tweet Posted Successfully");
+        }
+    });
 }
 
 function likeTweet(tweetId) {
-	client.post('favorites/create', {id: tweetId}, function(error, data, response) {
-		if(error) {
-			console.log("Something went Wrong!" + error);
-		} else {
-			console.log("Favorite Added Successfully");
-		}
-	});
+    client.post('favorites/create', { id: tweetId }, function(error, data, response) {
+        if (error) {
+            console.error(`Something went Wrong! ${error}`);
+        } else {
+            console.log("Favorite Added Successfully");
+        }
+    });
 }
 
 function befriendUser(userId) {
-	client.post('friendships/create', {user_id: userId}, function(error, data, response) {
-		if(error) {
-			console.log("Something went Wrong!" + error);
-		} else {
-			console.log("Friend Added Successfully");
-		}
-	});
+    client.post('friendships/create', { user_id: userId }, function(error, data, response) {
+        if (error) {
+            console.error(`Something went Wrong! ${error}`);
+        } else {
+            console.log("Friend Added Successfully");
+        }
+    });
 }
 
 var delay; //initializing variable for timeouts
 
+const ReplyMessages = function() {
+    this.textArray = [
+        'Hey, thanks for the follow! Tell me what you are coding at the moment...',
+        'Thank you for following me! Are you into coding too?',
+        'TYVM for following. Whats your favorite dev language?',
+        'Hello friend, have you read any cool articles lately related to coding?',
+        'Greetings, do you like JavaScript as much as I do?',
+        'Thanks for following! What piqued your interest in being a twitter-friend?',
+        'Sweet! Many thanks for following. What more would you like to hear from me on coding?'
+    ]
+    this.currentIndex = 0;
+}
+
+ReplyMessages.prototype.shuffle = function() {
+    this.queue = this.textArray.slice();
+    let len = this.queue.length - 1;
+    for (len; len > 0; len--) {
+        let randomIndex = Math.floor(Math.random() * (len + 1))
+        let text = this.queue[randomIndex];
+        this.queue[randomIndex] = this.queue[len];
+        this.queue[len] = text
+    }
+}
+
+ReplyMessages.prototype.replyText = function() {
+    return this.queue[currentIndex]
+}
+
+ReplyMessages.prototype.increment = function() {
+    this.currentIndex++;
+    if (this.currentIndex >= this.queue.length) {
+        this.currentIndex = 0;
+        this.shuffle();
+    }
+}
+
+const messageList = new ReplyMessages();
+messageList.shuffle();
 
 /*************************************/
 /****** STREAMING API LISTENERS ******/
 /*************************************/
 
 //listen on twitter stream for new follows
-stream.on('follow', function (event) {
+stream.on('follow', function(event) {
 
-	console.log("New Follow");
+    console.log("New Follow");
 
-	var name = event.source.name;
-	var screen_name = event.source.screen_name;
+    const name = event.source.name;
+    const screen_name = event.source.screen_name;
 
-	updateLogs(event, "follow.txt");
-	
-	if (screen_name != myId) {
-		console.log(`${name} @${screen_name} followed me.`);
+    if (screen_name != myId) {
+        console.log(`${name} @${screen_name} followed me.`);
 
-		var newTweet = {status: `@${screen_name} Thank you for following me! Are you into coding too? This is a #nodejsautoreply`};
-		// post a tweet in response to new follows
+        const message = messageList.replyText();
+        messageList.increment();
 
-		function send() {
-			postTweet(newTweet);
-		}
-		
-		delay = setTimeout(send, 4000);
-	}
+        const newTweet = { status: `@${screen_name} ${message} #loveNodeJS` };
+        // post a tweet in response to new follows
+
+        function send() {
+            postTweet(newTweet);
+        }
+
+        delay = setTimeout(send, 4000);
+    }
 });
 
 //constant stream of tweets from user's stream
-stream.on('tweet', function (event) {
-		
-	// var data = JSON.stringify(event, null, 5);
-	// fs.writeFile('botlog.txt', data, function(err){
-	// 	if (err){
-	// 		console.log("error occurred writing file: " + err);
-	// 	}	
-	// });
+stream.on('tweet', function(event) {
 
-	var replyto = event.in_reply_to_screen_name;
-	var tweetText = event.text;
-	var from = event.user.screen_name;
-	var fromId = event.user.id_str;
-	
-	//write most recent mention to a file, for records
-	if (tweetText.includes("@WesleyLHandy")) {
-		updateLogs(event, 'mention.txt');
-	}
-	//do something if current user in mentioned
-	if(replyto == myId || tweetText.includes("@WesleyLHandy")) {
+    const replyto = event.in_reply_to_screen_name;
+    const tweetText = event.text;
+    const from = event.user.screen_name;
+    const fromId = event.user.id_str;
 
-		var target_id = event.target_object.id_str;
+    //do something if current user in mentioned
+    if (replyto == myId || tweetText.includes("@WesleyLHandy")) {
 
-		function send() {
-			likeTweet(target_id);
-			befriendUser(from);
-		}
-		delay = setTimeout(send, 4000);
-	}
+        const target_id = event.target_object.id_str;
+
+        function send() {
+            likeTweet(target_id);
+            befriendUser(from);
+        }
+        delay = setTimeout(send, 4000);
+    }
 });
 
 //listen for when a tweet is favorited by another user
-stream.on('favorite', function (event) {
-  	//...
-  	console.log("Favorite Event");
-  	
-  	var from = event.source.screen_name;
-  	var fromId = event.source.id_str;
-	var target_user = event.target_object.user.screen_name;
-	var target_id = event.target_object.id_str;
-	var tweet_hashtags = event.target_object.entities.hashtags;
-	
-	console.log(`Favorite By ${from} on ${target_id}`);
-  	
-  	updateLogs(event, "favorite.txt");
-  	
-  	//follow any users who favorite one of my tweets
-  	if(target_user == myId && from != myId) {
-		
-		function send() {
-			befriendUser(fromId);
-		}
-		delay = setTimeout(send, 4000);
-	}
+stream.on('favorite', function(event) {
+    //...
+    console.log("Favorite Event");
+
+    const from = event.source.screen_name;
+    const fromId = event.source.id_str;
+    const target_user = event.target_object.user.screen_name;
+    const target_id = event.target_object.id_str;
+    const tweet_hashtags = event.target_object.entities.hashtags;
+
+    console.log(`Favorite By ${from} on https://www.twitter.com/${target_user}/status/${target_id}`);
+
+    //follow any users who favorite one of my tweets
+    if (target_user == myId && from != myId) {
+
+        function send() {
+            befriendUser(fromId);
+        }
+        delay = setTimeout(send, 4000);
+    }
 });
 
 //listen for quotes of my tweets
-stream.on('quoted_tweet', function (event) {
-	
-	console.log("Quote Event");
-	
-	var from = event.source.screen_name;
-	var fromId = event.source.id_str;
-	var target_user = event.target.screen_name;
-	var target_id = event.target_object.quoted_status_id_str;
-	var tweet_hashtags = event.target_object.entities.hashtags;
-	
-	console.log(`Quoted By ${from} on ${target_id}`);
-	
-	updateLogs(event, "quote.txt");
-	
-	//like the tweet and befriend the user who quotes my tweets
-	if(target_user == myId && from != myId) {
-		
-		function send() {
-			likeTweet(target_id);
-			befriendUser(fromId);
-		}
+stream.on('quoted_tweet', function(event) {
 
-		delay = setTimeout(send, 4000);
-	}
+    console.log("Quote Event");
+
+    const from = event.source.screen_name;
+    const fromId = event.source.id_str;
+    const target_user = event.target.screen_name;
+    const target_id = event.target_object.quoted_status_id_str;
+    const tweet_hashtags = event.target_object.entities.hashtags;
+
+    console.log(`Quoted By ${from} on https://www.twitter.com/${target_user}/status/${target_id}`);
+
+    //like the tweet and befriend the user who quotes my tweets
+    if (target_user == myId && from != myId) {
+
+        function send() {
+            likeTweet(target_id);
+            befriendUser(fromId);
+        }
+
+        delay = setTimeout(send, 4000);
+    }
 });
 
 //listen for retweets (quotes and retweets are similar, a quote will include some added content by the user)
 stream.on('retweeted_retweet', function(event) {
-	console.log("RT Event");
+    console.log("RT Event");
 
-	var from = event.source.screen_name;
-	var fromId = event.source.id_str;
-	var retweeted_from = event.target.screen_name;
-	var retweeted_object_id = event.target_object.id_str;
-	var retweeted_user = event.retweeted_status.user.screen_name;
-	var original_target_id = event.retweeted_status.id_str;
+    const from = event.source.screen_name;
+    const fromId = event.source.id_str;
+    const retweeted_from = event.target.screen_name;
+    const retweeted_object_id = event.target_object.id_str;
+    const retweeted_user = event.retweeted_status.user.screen_name;
+    const original_target_id = event.retweeted_status.id_str;
 
-	console.log(`Retweeted By ${from} on ${retweeted_object_id}`);
+    console.log(`Retweeted By ${from} on ${retweeted_object_id}`);
 
-	updateLogs(event, "rt.txt");
-	
-	//like the retweet and the original tweet then befriend user
-	if(target_user == myId && from != myId) {
-		
-		function send() {
-			likeTweet(retweeted_object_id);
-			likeTweet(original_target_id);
-			befriendUser(fromId);
-		}
-		delay = setTimeout(send, 4000);
-	}
+    //like the retweet and the original tweet then befriend user
+    if (target_user == myId && from != myId) {
+
+        function send() {
+            likeTweet(retweeted_object_id);
+            likeTweet(original_target_id);
+            befriendUser(fromId);
+        }
+        delay = setTimeout(send, 4000);
+    }
 });
-  
+
 stream.on('error', function(error) {
-	console.log("Stream" + error);
+    console.error(`Stream Error: ${error}`);
 });
-
-//function for adding tweets to the various logs without overwriting previous data
-function updateLogs(object, file) {
-
-	if (!fs.existsSync(file)) {
-		fs.writeFileSync(file, "[" + JSON.stringify(object) + "]");
-	} else {
-		fs.readFile(file, 'utf-8', (err, data) => {
-			if (err) {
-				console.log(err);
-			}
-
-			var arr = [];
-
-			if (data) {
-				arr = JSON.parse(data);
-			}
-
-			arr.push(object);
-
-			fs.writeFile(file, JSON.stringify(arr, null, 5), (err) => {
-					if (err) console.log(err);
-			});	
-		});
-	}
-}
